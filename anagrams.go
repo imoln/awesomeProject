ba7a5ca4deeb7c98d51d8c9b74a54cae48904e8a
imoln <imolnar0516@gmail.com>
2025-10-09 18:16:57 +0200

    cm_1

--- anagrams.go
+++ anagrams.go
@@ -18,13 +18,13 @@ func main() {
 	word1 = strings.ToLower(word1)
 	word2 = strings.ToLower(word2)
 
-	arr1 := strings.Split(word1, " ")
-	arr2 := strings.Split(word2, " ")
+	arr1 := strings.Split(word1, "")
+	arr2 := strings.Split(word2, "")
 
 	sort.Strings(arr1)
 	sort.Strings(arr2)
 
-	if strings.Join(arr1, " ") == strings.Join(arr2, " ") {
+	if strings.Join(arr1, "") == strings.Join(arr2, "") {
 		fmt.Println("Anagrams")
 	} else {
 		fmt.Println("Not Anagrams")
